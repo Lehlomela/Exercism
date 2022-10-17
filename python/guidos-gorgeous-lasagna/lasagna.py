fa45480924f76876d64236b0bb337d1cacb85a36
Lehlomela <lehlomela@psybergate.co.za>
2022-10-17 09:12:45 +0200

    guidos lasagna

--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,14 +3,14 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
+EXPECTED_BAKE_TIME = 40
+
+PREPARATION_TIME = 2
 
 
-# TODO: define the 'bake_time_remaining()' function
-def bake_time_remaining():
-    """Calculate the bake time remaining.
+def bake_time_remaining(elapsed_bake_time):
+    """
+    Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
     :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
@@ -19,12 +19,33 @@ def bake_time_remaining():
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
     """
-
-    pass
-
-
-# TODO: define the 'preparation_time_in_minutes()' function
-#       and consider using 'PREPARATION_TIME' here
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-# TODO: define the 'elapsed_time_in_minutes()' function
+def preparation_time_in_minutes(num_layers):
+    """
+    This function takes a parameter representing the number of layers in the lasagna
+    and returns preparation time
+
+    Args:
+        num_layers (int): layers in lasagna
+
+    Returns:
+        int: preparation time
+    """
+    return num_layers * PREPARATION_TIME
+
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """
+    This function takes two numbers representing the number of layers & the time already spent
+    baking and calculates the total elapsed minutes spent cooking the lasagna.
+
+    Args:
+        number_of_layers (int): layers in lasaga
+        elapsed_bake_time (int): elapsed time
+
+    Returns:
+        int: elapsed time
+    """
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
